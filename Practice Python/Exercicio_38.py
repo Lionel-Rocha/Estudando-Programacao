69e299e2882fac6b89c4d3fd89c55900427b54fb
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-04-03 13:39:26 -0300

    Exercício 38 e 39

--- Practice Python/Exercicio_38.py
+++ Practice Python/Exercicio_38.py
@@ -6,7 +6,7 @@ def calcula_cem_anos(nome, idade):
     ano_nascimento = int(ano) - int(idade)
     ano_cem_anos = int(ano_nascimento) + 100
     
-    print(f"Olá {nome}, você fará cem anos em {ano_cem_anos}")
+    print(f"Olá, {nome}, você fará cem anos em {ano_cem_anos}")
 
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
