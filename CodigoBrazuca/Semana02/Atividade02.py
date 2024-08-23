8c92a37e2b5f3d4f3b70ab08c259b2a6a2547f34
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-08-23 13:36:15 -0300

    Variáveis e Entrada de Usuário

---
+++ CodigoBrazuca/Semana02/Atividade02.py
@@ -0,0 +1,8 @@
+#Ninguém pediu para usar fString, mas eu sei usar, então usei
+def main():
+  filme = input("Qual é o seu filme favorito? ")
+  ano = input("Qual foi o ano de lançamento? ")
+
+  print(f"O seu filme favorito é {filme} e ele foi lançado em {ano}")
+
+main()
