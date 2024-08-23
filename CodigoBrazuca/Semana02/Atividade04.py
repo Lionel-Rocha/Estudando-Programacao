5580312b917b59f22bb1ea87bbf39ece2c843b07
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-08-23 13:41:30 -0300

    Estruturas Condicionais

---
+++ CodigoBrazuca/Semana02/Atividade04.py
@@ -0,0 +1,11 @@
+def main():
+  temperatura = float(input("Digite a temperatura atual: "))
+
+  if temperatura > 30:
+    print("Está quente")
+  elif temperatura < 15:
+    print("Está frio")
+  else:
+    print("Está agradável")
+
+main()
