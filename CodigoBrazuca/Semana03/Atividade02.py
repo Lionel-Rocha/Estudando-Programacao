57e7c6ae9f599b624a78254ca0da48f1dce8b115
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-08-26 10:47:10 -0300

    Conversão de Temperaturas

---
+++ CodigoBrazuca/Semana03/Atividade02.py
@@ -0,0 +1,7 @@
+def main():
+  celsius = float(input("Digite uma temperatura em Celsius: "))
+  kelvin = celsius + 273.15
+  fahrenheit = (celsius * 9/5) + 32
+
+  print(f"A temperatura em Kelvin é {kelvin} e Fahrenheit é {fahrenheit}")
+main()
